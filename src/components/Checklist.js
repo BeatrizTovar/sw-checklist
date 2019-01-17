81c4b9eec08a1d7a46f23bf37f020dafeac55118
Beatriz Tovar <beatrizctovar@gmail.com>
2019-01-17 14:59:55 -0800

    created Checklist component and added it App.js

---
+++ src/components/Checklist.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Checklist extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Coming from Checklist Component</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Checklist;
