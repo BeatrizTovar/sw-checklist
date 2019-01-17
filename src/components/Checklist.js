834d1af6b837ba85cc30303fa7548c564eb617f4
Beatriz Tovar <beatrizctovar@gmail.com>
2019-01-17 15:09:39 -0800

    adding array of titles

--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -1,6 +1,23 @@
 import React from "react";
 
 class Checklist extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            list = [
+                "Star Wars Episode IV - A New Hope",
+                "Star Wars Episode V - The Empire Strikes Back",
+                "Star Wars Episode VI - Return of the Jedi",
+                "Star Wars Episode I - THe Phantom Menace",
+                "Star Wars Episode II - Attack of the Clones",
+                "Star Wars Episode III - Revenge of the Sith",
+                "Star Wars Episode VII - The Force Awakens"
+            ]
+        }
+    };
+
+
   render() {
     return (
       <React.Fragment>
