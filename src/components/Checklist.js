c0a1f8327e8c4ea1f7a9e4ad25a54b0a7347e269
Beatriz Tovar <beatrizctovar@gmail.com>
2019-01-17 15:32:05 -0800

    adding unique keys to elements

--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -1,27 +1,36 @@
 import React from "react";
 
 class Checklist extends React.Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            list = [
-                "Star Wars Episode IV - A New Hope",
-                "Star Wars Episode V - The Empire Strikes Back",
-                "Star Wars Episode VI - Return of the Jedi",
-                "Star Wars Episode I - THe Phantom Menace",
-                "Star Wars Episode II - Attack of the Clones",
-                "Star Wars Episode III - Revenge of the Sith",
-                "Star Wars Episode VII - The Force Awakens"
-            ]
-        }
+    this.state = {
+      list: [
+        "Star Wars Episode IV - A New Hope",
+        "Star Wars Episode V - The Empire Strikes Back",
+        "Star Wars Episode VI - Return of the Jedi",
+        "Star Wars Episode I - THe Phantom Menace",
+        "Star Wars Episode II - Attack of the Clones",
+        "Star Wars Episode III - Revenge of the Sith",
+        "Star Wars Episode VII - The Force Awakens"
+      ]
     };
-
+  }
 
   render() {
+    let showList = this.state.list.map((entry, index) => {
+      return (
+        <div key={index}>
+          <input type="checkbox" />
+          {entry}
+        </div>
+      );
+    });
     return (
       <React.Fragment>
         <h1>Coming from Checklist Component</h1>
+        <em>List</em>
+        {showList}
       </React.Fragment>
     );
   }
