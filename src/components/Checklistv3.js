f688edaf61b49cd34788327b1dbdea1e9b18079e
Beatriz Tovar <beatrizctovar@gmail.com>
2019-01-18 12:35:30 -0800

    cleaning up comments and uncessary things

--- src/components/Checklistv3.js
+++ src/components/Checklistv3.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import { getMovies } from "../components/services/MovieService";
-import { Container } from 'semantic-ui-react'
-import '../Movies.css'
+import "../Movies.css"
 
 class Checklistv3 extends Component {
   constructor(props) {
     super();
     this.state = {
       movies: [],
-      checked: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -18,44 +16,32 @@ class Checklistv3 extends Component {
   }
 
   handleClick(e, index) {
-
-    console.log(this.state.movies[index])
-
     let obj = JSON.parse(JSON.stringify(this.state.movies))
     obj[index].isChecked = !obj[index].isChecked
     this.setState({ movies: obj })
-
-    // this.setState({
-    //   checked: e.target.checked
-    //   // checked: !this.state.checked
-    // });
-    // console.log(e.target.checked);
-    console.log(index)
   }
 
   render() {
 
 
     const movieList = this.state.movies.map((item, index) => {
-      let title = item.isChecked === true ? <em>{item.title}</em> : <span>{item.title}</span>
+      let title = item.isChecked === true ? <span className="checked">{item.title}</span> : <span>{item.title}</span>
 
       if (item.title == "Star Wars Episode IX") {
         return (
-        <div key={item.id} className="movie-entry">
-          <div >
-            <input className='movie-checkbox' key={item.id} type="checkbox" disabled />
-            <label>{title}</label>
-            <hr />
+          <div key={item.id} className="movie-entry">
+            <div >
+              <input className="movie-checkbox" key={item.id} type="checkbox" disabled />
+              <label>{title}</label>
+            </div>
           </div>
-        </div>
         )
       }
       return (
         <div key={item.id} className="movie-entry">
           <div>
-            <input className='movie-checkbox' key={item.id} type="checkbox" onClick={e => this.handleClick(e, index)} />
+            <input className="movie-checkbox" key={item.id} type="checkbox" onClick={e => this.handleClick(e, index)} />
             <label>{title}</label>
-            <hr />
           </div>
         </div>
       );
@@ -63,9 +49,8 @@ class Checklistv3 extends Component {
 
     return (
       <React.Fragment>
-        {console.log(this.state.movies)}
         <div className="movie-container title">
-        <span>CheckList</span>
+          <span>CheckList</span>
         </div>
         <div className="movie-container">
           <div>{movieList}</div>
